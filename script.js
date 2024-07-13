154c3be087ddc3cc4927d76568079f037094fc74
Shakir Ullah <shakirullahaup@gmail.com>
2024-07-13 23:27:27 +0500

    chap4 done

---
+++ script.js
@@ -0,0 +1,87 @@
+chapter no 4 naming rule
+
+rule nomber ( 1)
+   a variable can not contain any space 
+
+example:
+const my name =" shakir "    ( incorrect)
+
+const myName="shakir"  ( correct)
+
+Rull no (2)
+
+a variable name can contain only latter ,number.dollor$,underscore,
+
+
+Example:
+const  myName="shakir";
+
+const muName1="shakir"
+
+const my1Name="shakir"
+
+const $myName="shakir"
+
+const my$Name="shakir"
+
+const my_name="shakir"
+ 
+const _name="shakir"
+
+const my_first_name="shakir"
+
+this is the correct way 
+
+
+Rull no 3:
+
+note use in key word in code for example
+
+const const="my const" 
+
+this is in correct
+
+let let ="my car"
+
+this is in correct
+
+var var="shakir"
+
+this is in correct
+
+Rull No 4 :
+
+capital latter or fine but be carefull variable name is  case sensetio
+
+a bike is not a Bike like same spelling 
+
+example:
+
+const bike="mybike";
+console.log(bike);   (this is correct)
+
+const bike="mybike";
+console.log(Bike);
+
+let car="mycar"
+console.log(Car);( incorrect)
+console.log(car);
+
+Rull No 5
+
+came case meaning is all code second word first syntax capital
+
+for example :
+
+const myFirstName="shakir"
+console.log(myFirstName);
+
+const name ="shakir"
+console.log(name);
+
+const myLastName="khan"
+console.log(myLastName);
+
+done chap 4
+
+
